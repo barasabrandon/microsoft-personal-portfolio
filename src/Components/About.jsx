ec74d8799240491cf5ddccebacec9a9a3124ba8b
barasabrandon <87303910+barasabrandon@users.noreply.github.com>
2023-10-21 06:56:17 +0000

    made changes to about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm an Electrical and Telecommunications Engineering student studying at Moi University. I enjoy building intuitive Web applications.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
